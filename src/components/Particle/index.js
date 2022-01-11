68b5ee238a81774c70e5b2fc64dd937eab4c59a4
NhanTran <tranminhnhanhqng@gmail.com>
2022-01-12 00:36:07 +0700

    fix push on click

--- src/components/Particle/index.js
+++ src/components/Particle/index.js
@@ -29,7 +29,7 @@ export default function Particle() {
         interactivity: {
           events: {
             onClick: {
-              enable: true,
+              enable: false,
               mode: 'push',
             },
             onHover: {
